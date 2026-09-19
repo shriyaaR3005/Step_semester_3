673afbbd9a2016225e9081eae2a9e233c7e5dac7
shriyaaR3005 <sr4146@srmist.edu.in>
2026-09-19 10:44:52 +0530

    toydemo donr

---
+++ PRACTICE PROBLEMS/ToyDemo.java
@@ -0,0 +1,59 @@
+abstract class Toy {
+    private static int counter = 1000;
+    private final String toyId;
+
+    public Toy() {
+        counter++;
+        toyId = "TOY-" + counter;
+    }
+
+    public abstract String makeSound();
+
+    public String getToyId() {
+        return toyId;
+    }
+}
+
+class ToyCar extends Toy {
+    private String name;
+
+    public ToyCar(String name) {
+        super();
+        this.name = name;
+    }
+
+    @Override
+    public String makeSound() {
+        return name + ": Vroom vroom!";
+    }
+}
+
+class ToyRobot extends Toy {
+    private String name;
+
+    public ToyRobot(String name) {
+        super();
+        this.name = name;
+    }
+
+    @Override
+    public String makeSound() {
+        return name + ": Beep boop!";
+    }
+}
+
+public class ToyDemo {
+    public static void main(String[] args) {
+        ToyCar c = new ToyCar("Speedster");
+        ToyRobot r = new ToyRobot("Bolt");
+
+        System.out.println(c.makeSound());
+        System.out.println(r.makeSound());
+
+        System.out.println(c.getToyId());
+        System.out.println(r.getToyId());
+
+        // Toy t = new Toy(); 
+        // This will NOT compile because Toy is abstract.
+    }
+}
